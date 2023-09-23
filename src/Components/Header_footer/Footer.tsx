2f4e85ff7c946fe47a3b7e0d8633b3f5a1c27ca0
Chavidu <chavidukalpana40@gmail.com>
2023-09-23 18:27:43 +0530

    feat: footer

--- src/Components/Header_footer/Footer.tsx
+++ src/Components/Header_footer/Footer.tsx
@@ -1,8 +1,21 @@
-const Footer = ()=>{
-    return(
-        <>
-            Footer
-        </>
+import { CityLogo } from '../Utils/tools';
+
+
+const Footer = () => {
+    return (
+        <footer className='bck_blue'>
+            <div className='footer_logo'>
+                <CityLogo
+                    link={true}
+                    linkTo={'/'}
+                    width="70px"
+                    height="70px"
+                />
+            </div>
+            <div className='footer_descl'>
+                Manchester city 2023 All rights reserved
+            </div>
+        </footer>
     )
 }
 
