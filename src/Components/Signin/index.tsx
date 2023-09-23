725e41508203506a3be414868e278ceafbd750aa
Chavidu <chavidukalpana40@gmail.com>
2023-09-23 19:08:27 +0530

    feat: signin form

---
+++ src/Components/Signin/index.tsx
@@ -0,0 +1,80 @@
+import { useState } from "react";
+
+import { CircularProgress } from '@mui/material';
+import { Redirect } from "react-router-dom";;
+
+import { useFormik } from "formik";;
+import * as Yup from 'yup';
+
+
+const SignIn = () => {
+
+    const[loading,setLoading] = useState(false)
+
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: ''
+        },
+        validationSchema: Yup.object({
+            email: Yup.string()
+                .email('incalid email address')
+                .required('email is required'),
+            password: Yup.string()
+                .required('password is required')
+        }),
+        onSubmit: (values) => {
+            setLoading(true)
+            console.log(values)
+        }
+    })
+
+    return (
+        <div className="container">
+            <div className="signin_wrapper" style={{ margin: '100px' }}>
+                <form onSubmit={formik.handleSubmit}>
+                    <h2>Please login</h2>
+                    <input
+                        name="email"
+                        placeholder="email"
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        value={formik.values.email}
+                    />
+
+                    {formik.touched.email && formik.errors.email ?
+                        <div className="error_label">
+                            {formik.errors.email}
+                        </div> : null
+                    }
+                    <input
+                        name="password"
+                        placeholder="password"
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        value={formik.values.password}
+                    />
+
+                    {formik.touched.password && formik.errors.password ?
+                        <div className="error_label">
+                            {formik.errors.password}
+                        </div> : null
+                    }
+
+                    {
+                        loading?
+                            <CircularProgress color="secondary" className="progress" />
+                        :
+                        <button type="submit" >Login</button>
+                    }
+
+
+                    
+                </form>
+
+            </div>
+        </div>
+    )
+}
+
+export default SignIn
--- src/routes.tsx
+++ src/routes.tsx
@@ -3,6 +3,7 @@ import {Switch,Route,BrowserRouter} from 'react-router-dom';
 import Header from './Components/Header_footer/Header';
 import Footer from './Components/Header_footer/Footer';
 import Home from './Components/Home';
+import SignIn from './Components/Signin'
 
 const Routes =()=> {
   
@@ -10,6 +11,7 @@ const Routes =()=> {
     <BrowserRouter>
     <Header/>
       <Switch>
+        <Route path="/sign_in" exact component={SignIn}/>
         <Route path="/" exact component={Home}/>
       </Switch>
     <Footer/>
